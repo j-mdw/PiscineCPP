65cf9b8070edb73fd9592a8024247abc3b1dbba2
jmaydew <jmaydew@student.42.fr>
2021-05-16 23:26:21 +0200

    +ex00 output fomatting:

--- d06/ex00/main.cpp
+++ d06/ex00/main.cpp
@@ -1,7 +1,11 @@
 # include <string>
+# include <cstring>
 # include <iostream>
 # include <limits>
 # include <cmath>
+# include <cstdlib>
+# include <iomanip>
+# include <ios>
 
 # define TYPE_CHAR 0
 # define TYPE_INT 1
@@ -40,7 +44,7 @@ special_case(std::string & s)
         "nan"
         };
 
-    if (s.size() == 1)
+    if (s.size() == 1 && !std::isdigit(*s.begin()))
         return (TYPE_CHAR);
     for (int i = 0; i < 6; i++)
     {
@@ -77,7 +81,7 @@ get_type(std::string & s)
         return (TYPE_INT);
     else if (it == end && dot_count == 1)
         return (TYPE_DOUBLE);
-    else if (std::isdigit(*(it - 1)) && it == (end - 1) && *it == 'f')
+    else if (std::isdigit(*(it - 1)) && it == (end - 1) && *it == 'f' && dot_count == 1)
         return (TYPE_FLOAT);
     return (-1);
 } 
@@ -86,12 +90,12 @@ void
 print_from_char(char c)
 {
     if (!std::isprint(c))
-        std::cout << "char: " << "impossible" << std::endl;
+        std::cout << "char: " << "Non displayable" << std::endl;
     else
     {
         std::cout << "char: " << c << std::endl;
         std::cout << "int: " << static_cast<int>(c) << std::endl;
-        std::cout << "float: " << static_cast<float>(c) << std::endl;
+        std::cout << "float: " << static_cast<float>(c) << 'f' << std::endl;
         std::cout << "double: " << static_cast<double>(c) << std::endl;
     }
 }
@@ -99,29 +103,32 @@ print_from_char(char c)
 void
 print_from_int(int i)
 {
-    if (!std::isprint(i))
+    if (i > std::numeric_limits<char>::max() || i < 0)
         std::cout << "char: " << "impossible" << std::endl;
+    else if (!std::isprint(static_cast<int>(i)))
+        std::cout << "char: " << "Non displayable" << std::endl;
     else
         std::cout << "char: " << static_cast<char>(i) << std::endl;
 
     std::cout << "int: " << static_cast<int>(i) << std::endl;
-    std::cout << "float: " << static_cast<float>(i) << std::endl;
+    std::cout << "float: " << static_cast<float>(i) << 'f' << std::endl;
     std::cout << "double: " << static_cast<double>(i) << std::endl;
 }
 
 void
 print_from_float(float f)
 {
-   int i;
+    int i;
 
-        std::cout << f << '\n';
     if (f > std::numeric_limits<int>::min()
         && f < std::numeric_limits<int>::max())
     {
         i = static_cast<int>(f);
         std::cout << f << '\n';
-        if (!std::isprint(static_cast<int>(f)))
+        if (i > std::numeric_limits<char>::max() || i < 0)
             std::cout << "char: " << "impossible" << std::endl;
+        else if (!std::isprint(static_cast<int>(i)))
+            std::cout << "char: " << "Non displayable" << std::endl;
         else
             std::cout << "char: " << static_cast<char>(f) << std::endl;
         std::cout << "int: " << static_cast<int>(f) << std::endl;
@@ -131,7 +138,7 @@ print_from_float(float f)
         std::cout << "char: " << "impossible" << std::endl;
         std::cout << "int: " << "impossible" << std::endl;
     }
-    std::cout << "float: " << f << std::endl;
+    std::cout << "float: " << f << 'f' << std::endl;
     std::cout << "double: " << static_cast<double>(f) << std::endl;
 }
 
@@ -144,8 +151,10 @@ print_from_double(double d)
         && d <= std::numeric_limits<int>::max())
     {
         i = static_cast<int>(d);
-        if (!std::isprint(i))
+        if (i > std::numeric_limits<char>::max() || i < 0)
             std::cout << "char: " << "impossible" << std::endl;
+        else if (!std::isprint(static_cast<int>(i)))
+            std::cout << "char: " << "Non displayable" << std::endl;
         else
             std::cout << "char: " << static_cast<char>(d) << std::endl;
         std::cout << "int: " << i << std::endl;
@@ -155,11 +164,12 @@ print_from_double(double d)
         std::cout << "char: " << "impossible" << std::endl;
         std::cout << "int: " << "impossible" << std::endl;
     }
-    if (d > std::numeric_limits<float>::max()
+    if ((d > std::numeric_limits<float>::max()
     || d < std::numeric_limits<float>::min())
+    && d != 0)
         std::cout << "float: " << "impossible" << std::endl;
     else
-        std::cout << "float: " << static_cast<float>(d) << std::endl;
+        std::cout << "float: " << static_cast<float>(d) << 'f' << std::endl;
     std::cout << "double: " << d << std::endl;
 }
 
@@ -181,7 +191,6 @@ convert_print(std::string s)
 
 
     type = get_type(s);
-    std::cout << "Type: " << type << std::endl;
     if (type < 0)
     {
         std::cout << "Input is neither, char, int, float or double convertible" << std::endl;
@@ -228,5 +237,7 @@ main(int ac, char **av)
         std::cout << "Provide 1 input" << std::endl;
         return (1);
     }
-    return (convert_print(av[1]));
+	std::cout << std::fixed;
+	std::cout << std::setprecision(1);
+	return (convert_print(av[1]));
 }
