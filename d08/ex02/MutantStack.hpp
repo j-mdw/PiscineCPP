80242e6a8c50665d80efb09f65f000ec25d29ff3
Julien Maydew <MDW@MacBook-Pro-de-Julien.local>
2021-05-19 23:01:38 +0200

    + d08 ex02

--- d08/ex02/MutantStack.hpp
+++ d08/ex02/MutantStack.hpp
@@ -2,45 +2,26 @@
 # define MUTANTSTACK_H
 
 # include <stack>
-# include <iterator>
+# include <queue>
 
 template <typename T>
 class MutantStack : public std::stack<T> {
 
 public:
+    typedef typename std::stack<T>::container_type::iterator iterator;
 
     MutantStack(void) {};
     MutantStack(MutantStack const & src) : std::stack<T>(src) {};
     ~MutantStack(void) {};
     MutantStack &  operator=(MutantStack const & src) { src = std::stack<T>::operator=(src); };
 
-    class iterator {
-        public:
-            iterator(void) : _valAdr(NULL) {};
-            iterator(T * val) : _valAdr(val) {};
-            iterator(iterator const & src) : _valAdr(src.getVal())  {};
-            ~iterator(void) {};
-            iterator & operator=(iterator const & src) { this->_valAdr = src.getVal(); };
-
-            T * getVal(void) const { return (this->_valAdr); };
-            iterator operator++(void) { return (--this->_valAdr); };
-            iterator operator--(void) { return (++this->_valAdr); };
-            iterator operator++(int) { return (--this->_valAdr); };
-            iterator operator--(int) { return (++this->_valAdr); }; 
-            int operator*(void) const { return (*this->_valAdr); };
-
-        private:
-            T *_valAdr;
-    };
-    iterator begin(void) const
+    iterator begin(void)
     {
-        iterator it( &const_cast<T &>( this->top() ) );
-        return (it);
+        return (std::stack<T>::c.begin()) ;
     };
-    iterator end(void) const
+    iterator end(void)
     {
-        iterator it( &( const_cast<T &>( this->top() ) ) - this->size() );
-        return (it);
+        return (std::stack<T>::c.end());
     };
 };
 
--- d08/ex02/main.cpp
+++ d08/ex02/main.cpp
@@ -1,29 +1,56 @@
-#include "MutantStack.hpp"
+#include "mutantstack.hpp"
 #include <iostream>
 
 int
 main(void)
 {
-    MutantStack<int> ms;
+    std::cout << "\n\nMy-tests\n" << std::endl;
+    {
+        MutantStack<int> ms;
 
-    ms.push(10);
-    ms.push(5);
-    std::cout << ms.top() << std::endl;
+        ms.push(10);
+        ms.push(5);
+        std::cout << ms.top() << std::endl;
 
-    MutantStack<int>::iterator it =  ms.begin();
-    std::cout << *it << std::endl;
-    std::cout << *(--it) << std::endl;
-    std::cout << *(++it) << std::endl;
-    std::cout << *(++it) << std::endl;
+        MutantStack<int>::iterator it =  ms.begin();
+        std::cout << *it << std::endl;
+        std::cout << *(++it) << std::endl;
+        ms.push(707);
+        ms.push(107);
+        std::cout << *(++it) << std::endl;
+        std::cout << *(++it) << std::endl;
 
-    
-    MutantStack<int>::iterator ite =  ms.end();
-    std::cout << *ite << std::endl;
-    std::cout << *(++ite) << std::endl;
-    std::cout << *(--ite) << std::endl;
-    std::cout << *(--ite) << std::endl;
-    ms.push(8);
-    std::cout << *(--ite) << std::endl;
-
-
+        
+        MutantStack<int>::iterator ite =  ms.end();
+        std::cout << *ite << std::endl;
+        std::cout << *(--ite) << std::endl;
+        std::cout << *(--ite) << std::endl;
+        std::cout << *(--ite) << std::endl;
+        ms.push(8);
+        std::cout << *(--ite) << std::endl;
+    }
+    std::cout << "\n\n42-tests\n" << std::endl;
+    {
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
+        std::cout << mstack.top() << std::endl;
+        mstack.pop();
+        std::cout << mstack.size() << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::stack<int> s(mstack);
+    }
+    return 0;
 }
